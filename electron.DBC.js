5b67b7e85dfefac5d742a2ab4afe62e70bd41728
Amber <pinkiepief1@gmail.com>
2026-02-24 15:33:46 -0500

    Change request origin URL for web request

--- electron.DBC.js
+++ electron.DBC.js
@@ -21,7 +21,7 @@ async function createWindow() {
 	};
 	var { session } = win.webContents;
 	session.webRequest.onBeforeSendHeaders(filter, (details, callback) => {
-		details.requestHeaders["origin"] = "https://hummus.sys42.net";
+		details.requestHeaders["origin"] = "https://staging.oldcordapp.com";
 		delete details.requestHeaders["User-Agent"];
 		callback({ requestHeaders: details.requestHeaders });
 	});
